2396a811f140f1922b574d5f68d6be2c0533e617
Steve <klop9090@g.skku.edu>
2022-02-09 13:12:05 +0900

    0209 Complete

---
+++ before/0209/11722.py
@@ -0,0 +1,14 @@
+n = int(input())
+
+arr = list(map(int, input().split()))
+
+ans = [0] * n
+
+for i in range(n):
+    for j in range(i):
+        if arr[i] < arr[j] and ans[i] < ans[j]:
+            ans[i] = ans[j]
+    ans[i] += 1
+    
+
+print(max(ans))
